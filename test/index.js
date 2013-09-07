cbcaa9a09b5d647a730189ad879d3087e65fe568
Eugene Ware <eugene@noblesamurai.com>
2013-09-07 22:10:02 +1000

    basic level example working:

--- test/index.js
+++ test/index.js
@@ -1,7 +1,16 @@
 var expect = require('expect.js'),
+    level = require('level'),
+    path = require('path'),
+    rimraf = require('rimraf'),
     hex = require('..');
 
 describe('bytewise-hex', function() {
+  var dbPath = path.join(__dirname, '..', 'data', 'testdb');
+  beforeEach(function(done) {
+    rimraf.sync(dbPath);
+    done();
+  });
+
   it('should be able to do some basic encoding', function(done) {
     var text = 'hello world';
     var encoded = hex.encode(text);
@@ -9,5 +18,18 @@ describe('bytewise-hex', function() {
     var decoded = hex.decode(encoded);
     expect(decoded).to.equal(text);
     done();
+  });
+
+  it('should be able to create a db with the hex encoding', function(done) {
+    var db = level(dbPath, { keyEncoding: hex, valueEncoding: 'json' });
+    db.put(['hello', 'world', 123], { something: 'here' }, get);
+    function get(err) {
+      if (err) return done(err);
+      db.get(['hello', 'world', 123], function (err, data) {
+        if (err) return done(err);
+        expect(data).to.eql({ something: 'here' });
+        db.close(done);
+      });
+    }
   });
 });
